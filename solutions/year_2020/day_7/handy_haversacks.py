78ef428f9cd218a4efe75f60e92e4bd23686ff5d
Meulen <stef.van.der.meulen@ordina.nl>
2020-12-07 15:30:07 +0100

    Add solution to part one

--- solutions/year_2020/day_7/handy_haversacks.py
+++ solutions/year_2020/day_7/handy_haversacks.py
@@ -19,7 +19,7 @@ def parse_data(data: List[str]) -> dict:
     data = [[item.strip(" |.") for item in rule] for rule in data]
     data_dict = {rule[0]: rule[1:] for rule in data}
 
-    data_dict = unpack_bag_rules(rules=data_dict)
+    # data_dict = unpack_bag_rules(rules=data_dict)
 
     return data_dict
 
@@ -34,12 +34,23 @@ def parse_bag(bag: str) -> (str, int):
     return bag_renamed, bag_count
 
 
-def unpack(rules: dict, outer_bag: str, inner_bags: str, bags_unpacked: list = None) -> list:
+def find_bag_type(data: dict, bag_type: str = "shiny gold bag", bags_gold: list = None) -> list:
+
+    bags_gold = bags_gold if bags_gold is not None else []
+
+    bags_containing_type = [key for key, lst in data.items() if any([bag_type in val for val in lst])]
+    bags_containing_type = [bag for bag in bags_containing_type if bag not in bags_gold]
+    if bags_containing_type:
+        bags_gold.extend(bags_containing_type)
+        for bag in bags_containing_type:
+            bags_gold = find_bag_type(data=data, bag_type=bag.rstrip("s"), bags_gold=bags_gold)
+
+    return bags_gold
+
+
+def unpack(rules: dict, inner_bags: str, bags_unpacked: list = None) -> list:
 
     bags_unpacked = bags_unpacked if bags_unpacked is not None else []
-
-    if len(bags_unpacked) > 10000:
-        return bags_unpacked
 
     for bag in inner_bags:
 
@@ -51,17 +62,16 @@ def unpack(rules: dict, outer_bag: str, inner_bags: str, bags_unpacked: list = N
         if bag in bags_unpacked:
             continue
 
-        if rules[bag] == ["no other bags"]:
-            bags_unpacked.extend(bag_count * [bag])
-            return bags_unpacked
-
         if bag in rules:
-            for _ in range(bag_count):
-                bags_unpacked.append(bag)
-                bags_unpacked = unpack(rules=rules, outer_bag=bag, inner_bags=rules[bag], bags_unpacked=bags_unpacked)
+            bags_unpacked.append(bag)
 
-                if "shiny gold bags" in bags_unpacked:
-                    return bags_unpacked
+            if "shiny gold bags" in bags_unpacked:
+                return bags_unpacked
+
+            if rules[bag] == ["no other bags"]:
+                return bags_unpacked
+
+            bags_unpacked = unpack(rules=rules, inner_bags=rules[bag], bags_unpacked=bags_unpacked)
 
     return bags_unpacked
 
@@ -71,7 +81,7 @@ def unpack_bag_rules(rules: dict) -> dict:
     rules_unpacked = deepcopy(rules)
 
     for outermost, underlying in rules.items():
-        rules_unpacked[outermost] = unpack(rules=rules, outer_bag=outermost, inner_bags=underlying)
+        rules_unpacked[outermost] = unpack(rules=rules, inner_bags=underlying)
 
     return rules_unpacked
 
@@ -90,12 +100,16 @@ def main():
     config = Config()
     path_file = os.path.join(config.path_data, "day_7", "bag_color_rules_test.txt")
     data_test = read_txt_file(path_file=path_file)
-    n_bags_carrying_shiny_gold = compute_part_one(data=data_test)
+
+    data_test_parsed = parse_data(data=data_test)
+    bags_carrying_shiny_gold = find_bag_type(data=data_test_parsed, bag_type="shiny gold bag")
+    n_bags_carrying_shiny_gold = len(bags_carrying_shiny_gold)
     assert 4 == n_bags_carrying_shiny_gold
 
     path_file = os.path.join(config.path_data, "day_7", "bag_color_rules.txt")
     data = read_txt_file(path_file=path_file)
-    n_bags_carrying_shiny_gold = compute_part_one(data=data)
+    data_parsed = parse_data(data=data)
+    n_bags_carrying_shiny_gold = find_bag_type(data=data_parsed)
     # assert 4 == n_bags_carrying_shiny_gold
 
     return True
