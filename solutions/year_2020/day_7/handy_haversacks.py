4556e96096c9674307748589b5a0cab572e38a96
Meulen <stef.van.der.meulen@ordina.nl>
2020-12-07 14:34:24 +0100

    Working solution for test set

--- solutions/year_2020/day_7/handy_haversacks.py
+++ solutions/year_2020/day_7/handy_haversacks.py
@@ -6,6 +6,7 @@ Date: 07/12/2020
 
 import os
 import re
+from copy import deepcopy
 from typing import List
 
 from solutions.config import Config
@@ -23,40 +24,65 @@ def parse_data(data: List[str]) -> dict:
     return data_dict
 
 
-def unpack_bag_rules(rules: dict, subrules: dict = None) -> dict:
+def parse_bag(bag: str) -> (str, int):
 
-    subrules = subrules if subrules is not None else rules
+    bag_count, = re.findall("^\d+", bag)
+    bag_count = int(bag_count)
+    bag_renamed = re.sub('^\d+\s+', "", bag)
+    if bag_count < 2:
+        bag_renamed += "s"
+    return bag_renamed, bag_count
 
-    for outermost, underlying in subrules.items():
-        for bag in underlying:
 
-            if isinstance(bag, dict):
-                subrules[outermost].extend(unpack_bag_rules(rules=rules, subrules=bag))
-                subrules[outermost].remove(bag_renamed)
-                continue
-            if bag == "no other bags":
-                continue
+def unpack(rules: dict, outer_bag: str, inner_bags: str, bags_unpacked: list = None) -> list:
 
-            bag_count, = re.findall("^\d+", bag)
-            bag_count = int(bag_count)
-            bag_renamed = re.sub('^\d+\s+', "", bag)
-            if bag_count < 2:
-                bag_renamed += "s"
-            if bag_renamed in rules.keys():
-                subrules[outermost].remove(bag)
-                if subrules != rules:
-                    subrules[outermost] = {bag_renamed: bag_count * rules[bag_renamed]}
-                else:
-                    subrules[outermost].extend([{bag_renamed: bag_count * rules[bag_renamed]}])
+    bags_unpacked = bags_unpacked if bags_unpacked is not None else []
 
-    return subrules
+    if len(bags_unpacked) > 10000:
+        return bags_unpacked
+
+    for bag in inner_bags:
+
+        if bag == "no other bags":
+            return bags_unpacked
+
+        bag, bag_count = parse_bag(bag=bag)
+
+        if bag in bags_unpacked:
+            continue
+
+        if rules[bag] == ["no other bags"]:
+            bags_unpacked.extend(bag_count * [bag])
+            return bags_unpacked
+
+        if bag in rules:
+            for _ in range(bag_count):
+                bags_unpacked.append(bag)
+                bags_unpacked = unpack(rules=rules, outer_bag=bag, inner_bags=rules[bag], bags_unpacked=bags_unpacked)
+
+                if "shiny gold bags" in bags_unpacked:
+                    return bags_unpacked
+
+    return bags_unpacked
+
+
+def unpack_bag_rules(rules: dict) -> dict:
+
+    rules_unpacked = deepcopy(rules)
+
+    for outermost, underlying in rules.items():
+        rules_unpacked[outermost] = unpack(rules=rules, outer_bag=outermost, inner_bags=underlying)
+
+    return rules_unpacked
 
 
 def compute_part_one(data: list) -> int:
 
-    rules = parse_data(data=data)
+    rules_unpacked = parse_data(data=data)
 
-    return data
+    bags_carrying_shiny_gold = [key for key, val in rules_unpacked.items() if "shiny gold bags" in val]
+
+    return len(bags_carrying_shiny_gold)
 
 
 def main():
@@ -66,6 +92,11 @@ def main():
     data_test = read_txt_file(path_file=path_file)
     n_bags_carrying_shiny_gold = compute_part_one(data=data_test)
     assert 4 == n_bags_carrying_shiny_gold
+
+    path_file = os.path.join(config.path_data, "day_7", "bag_color_rules.txt")
+    data = read_txt_file(path_file=path_file)
+    n_bags_carrying_shiny_gold = compute_part_one(data=data)
+    # assert 4 == n_bags_carrying_shiny_gold
 
     return True
 
