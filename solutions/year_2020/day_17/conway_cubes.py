8af9d835839934bed8b438d183f6d69191c941f2
Meulen <stef.van.der.meulen@ordina.nl>
2020-12-22 00:10:01 +0100

    Add solution day 17 part two

--- solutions/year_2020/day_17/conway_cubes.py
+++ solutions/year_2020/day_17/conway_cubes.py
@@ -3,100 +3,70 @@
 Written by: sme30393
 Date: 21/12/2020
 """
-import re
-from copy import deepcopy
-
+import itertools
 import numpy as np
 import os
+import re
 
+from copy import deepcopy
 from solutions.config import Config
 from solutions.year_2020.utils.file_manager import read_txt_file
 
 
-def update_cube_state(state: np.ndarray, coord: tuple) -> int:
-    x, y, z = coord
+def update_cube_state(state: np.ndarray, coordinates: tuple) -> int:
+    coords_neighbors = itertools.product(
+        *[list(range(c - 1, min(c + 2, state.shape[i]))) for i, c in enumerate(coordinates)])
 
-    neighbors = [
-        [
-            [
-                (abs(i), abs(j), abs(k)) for k in range(z - 1, min(z + 2, state.shape[2]))
-            ]
-            for j in range(y - 1, min(y + 2, state.shape[1]))
-        ]
-        for i in range(x - 1, min(x + 2, state.shape[0]))
-    ]
-    neighbors = [group for subgroup in neighbors for group in subgroup]
-    neighbors = [group for subgroup in neighbors for group in subgroup if group != coord]
-    neighbors = list(set(neighbors))
+    coords_neighbors = [n for n in coords_neighbors if not any([v < 0 for v in n]) and n != coordinates]
 
-    state_new = deepcopy(state)
-
-    states_active = np.sum(state[tuple(np.moveaxis(np.array(neighbors), -1, 0))])
-    if state_new[x, y, z] == 1 and states_active not in (2, 3):
+    states_active = np.sum(state[tuple(np.moveaxis(np.array(coords_neighbors), -1, 0))])
+    if state[coordinates] == 1 and states_active not in (2, 3):
         return 0
-    elif state_new[x, y, z] == 0 and states_active == 3:
+    elif state[coordinates] == 0 and states_active == 3:
         return 1
 
-    return state_new[x, y, z]
+    return state[coordinates]
 
 
-def simulate_cycles(state_initial: np.ndarray, n_cycles: int = 6) -> np.ndarray:
-    assert 3 == len(state_initial.shape)
+def simulate_cycles(state_initial: np.ndarray, n_cycles: int = 6, dims: int = 3) -> np.ndarray:
     state_final = deepcopy(state_initial)
-    while state_initial.shape[2] < (n_cycles*2) + 3:
-        state_expanded = expand_state(state=state_initial)
-        x, y, z = state_expanded.shape
-
+    while state_initial.shape[-1] < (n_cycles * 2) + 3:
+        state_expanded = expand_state(state=state_initial, dims=dims)
         state_final = deepcopy(state_expanded)
-        for i in range(x):
-            for j in range(y):
-                for k in range(z):
-                    state_final[i, j, k] = update_cube_state(state=state_expanded, coord=(i, j, k))
+        for coord in itertools.product(*[list(range(i)) for i in state_final.shape]):
+            state_final[coord] = update_cube_state(state=state_expanded, coordinates=coord)
 
-        # spaceviewer(state_final)
         return simulate_cycles(state_initial=state_final, n_cycles=n_cycles)
 
     return state_final
 
 
-def parse_initial_state(path_file: str) -> np.ndarray:
+def parse_initial_state(path_file: str, dims: int = 3) -> np.ndarray:
     data = read_txt_file(path_file=path_file)
     data = [re.split("", line)[1:-1] for line in data]
     data = np.array(data)
-    data = data.reshape([data.shape[0], data.shape[1], 1])
+
+    data_shape_new = [s for s in data.shape]
+    data_shape_new = data_shape_new if len(data_shape_new) == dims else data_shape_new + [1] * (dims - len(data_shape_new))
+    data = data.reshape(data_shape_new)
     data[data == "#"] = 1
     data[data == "."] = 0
-    data.astype(int)
-
+    data = data.astype(int)
     data = expand_state(state=data)
-    spaceviewer(data)
     return data
 
 
-def expand_state(state: np.ndarray) -> np.ndarray:
-    x_init, y_init, z_init = state.shape
+def expand_state(state: np.ndarray, dims: int = 3) -> np.ndarray:
+    coords_init = state.shape
+    coords_expand = [c + 2 if c != 0 else c for c in coords_init]
+    coords_expand_offset = [(c_exp - c_init) // 2 for c_exp, c_init in zip(coords_expand, coords_init)]
 
-    x = x_init + 2 if state.shape[2] != 1 else x_init
-    y = y_init + 2 if state.shape[2] != 1 else y_init
-    z = z_init + 2
+    state_expanded = np.zeros(coords_expand).astype(int)
 
-    x_offset = (x - x_init) // 2
-    y_offset = (y - y_init) // 2
-    z_offset = (z - z_init) // 2 if state.shape[2] != 1 else 1
-    state_expanded = np.zeros([x, y, z]).astype(int)
+    slicing = [slice(c_offset, c - c_offset) if c != 1 else slice(c_offset) for c_offset, c in zip(coords_expand_offset, coords_expand)]
 
-    state_expanded[x_offset:x - x_offset, y_offset:y - y_offset, z_offset:z - z_offset] = state
+    state_expanded[tuple(slicing)] = state
     return state_expanded
-
-
-def spaceviewer(space: np.ndarray):  # Take a look at the current state of the system
-    c = 0
-    for z in np.rollaxis(space, axis=2):
-        print(c)
-        c += 1
-        [print(y) for y in z]
-
-    return True
 
 
 def main():
@@ -109,9 +79,23 @@ def main():
     state_final = simulate_cycles(state_initial=state_initial, n_cycles=6)
     assert 112 == state_final.sum().sum()
 
+    # Real deal
     state_initial = parse_initial_state(path_file=os.path.join(config.path_data, 'initial_state.txt'))
     state_final = simulate_cycles(state_initial=state_initial, n_cycles=6)
     print(f"The number of active states upon 6 cycles equals: {state_final.sum().sum()}")
+    assert 255 == state_final.sum().sum()
+
+    # PART TWO
+
+    # Test
+    state_initial = parse_initial_state(path_file=os.path.join(config.path_data, 'initial_state_test.txt'), dims=4)
+    state_final = simulate_cycles(state_initial=state_initial, n_cycles=6, dims=4)
+    assert 848 == state_final.sum().sum()
+
+    # Real deal
+    state_initial = parse_initial_state(path_file=os.path.join(config.path_data, 'initial_state.txt'), dims=4)
+    state_final = simulate_cycles(state_initial=state_initial, n_cycles=6, dims=4)
+    print(f"PART TWO: The number of active states upon 6 cycles equals: {state_final.sum().sum()}")
 
     return True
 
